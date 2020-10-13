113a381ebcb711920d68725463cbb87934880fbe
Ozaniak <Ozaniak@PC>
2020-10-13 15:10:30 +0200

    hotovo

--- Open-Lab-02.02/Formatter.cs
+++ Open-Lab-02.02/Formatter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Open_Lab_02._02
 {
@@ -6,7 +7,16 @@ namespace Open_Lab_02._02
     {
         public void Format(double[] numbers)
         {
-            throw new NotImplementedException();
+           for (int i = 0; i < 5; i++)
+            {
+                NumberFormatInfo nfi = new NumberFormatInfo();
+                nfi.NumberDecimalSeparator = ".";
+                double numbersdecimal = numbers[i];
+                numbersdecimal = Math.Round(numbersdecimal, 2);
+                Console.Write(numbersdecimal.ToString(nfi));
+                if (i < 4) Console.Write(" ");
+
+            }
         }
     }
 }
